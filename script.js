a7383b27ac6d5029db2c2253cc761a4e6703ef28
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2026-02-27 23:05:24 +0000

    fetch and display default user details including all repos

--- script.js
+++ script.js
@@ -1,126 +1,120 @@
 let search_input = document.getElementById("search-input");
 let result_container = document.getElementById("content-section");
 let side_bar = document.getElementById("my-sidebar");
-console.log(side_bar)
+let repos_list = document.getElementById("repos-list");
+console.log(side_bar);
 let timeoutId;
 let isLoading = false;
 let cache = {};
 
-
-
-
-async function fetchData(url){
-    try{
-        let result = await fetch(url);
-        return result;
-    }
-    catch(err){
-        console.log(err);
-    }
+async function fetchData(url) {
+  try {
+    let result = await fetch(url);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function debounce(cb){
-    clearTimeout(timeoutId);
+function debounce(cb) {
+  clearTimeout(timeoutId);
 
-    timeoutId = setTimeout(() => {
-            cb()
-    }, 2000);
+  timeoutId = setTimeout(() => {
+    cb();
+  }, 2000);
 }
 
-// handles search 
-function handleSearch(search_term, cb){
-    let url;
+// handles search
+function handleSearch(search_term, cb) {
+  let url;
 
-    if(search_term === ""){
-            url = `https://api.github.com/users/boakaidorborkamara`;
-    }
-    else{
-        url = `https://api.github.com/users/${search_term}`;
-    }
+  if (search_term === "") {
+    url = `https://api.github.com/users/boakaidorborkamara`;
+  } else {
+    url = `https://api.github.com/users/${search_term}`;
+  }
 
-    
+  isLoading = true;
+  cb(isLoading, null);
 
-    isLoading = true;
-    cb(isLoading, null);
+  // check cache
+  if (cache[search_term]) {
+    console.log("loading cached user...");
+    isLoading = false;
 
-     // check cache 
-            if(cache[search_term]){
-                console.log("loading cached user...")
-                isLoading = false;
+    let cached_user = cache[search_term];
 
-                let cached_user = cache[search_term];
+    cb(isLoading, cached_user);
+    return;
+  } else
+    debounce(() => {
+      fetchData(url)
+        .then((response) => response.json())
+        .then((data) => {
+          // cache new data
+          cache[data.login] = data;
+          console.log("cache", cache);
 
-                cb(isLoading, cached_user);
-                return;
-            }else debounce(() => {
+          isLoading = false;
+          cb(isLoading, data);
+        });
+    });
+}
 
-           
+// handles what displays when the app first loads
+function handleFirstLoad(cb) {
+  let url = `https://api.github.com/users/boakaidorborkamara`;
 
-            fetchData(url)
-            .then(response => response.json())
-            .then(data =>{
-                // cache new data 
-                cache[data.login] = data;
-                console.log("cache", cache);
+  isLoading = true;
+  cb(isLoading, null);
 
-                isLoading = false;
-                cb(isLoading, data);
-            });
-            
+  fetchData(url)
+    .then (response => response.json())
+    .then((user) => {
+      return fetchData(user.repos_url)
+        .then(response => response.json())
+        .then((repos) => {
+          return { user, repos };
+        });
     })
-    
+    .then((result) => {
+      isLoading = false;
+      cb(isLoading, result);
+      return result;
+    });
 }
 
-// handles what displays when the app first loads 
-function handleFirstLoad(cb){
-    let url = `https://api.github.com/users/boakaidorborkamara`;
+// handles display of data to the DOM
+function renderUI(isLoading, result) {
+  console.log("kdkdkdrrrr");
+  console.log("resulttt", result);
 
-    isLoading = true;
-    cb(isLoading, null);
-
-    fetchData(url)
-            .then(response => response.json())
-            .then(data =>{
-                isLoading = false;
-                cb(isLoading, data);
-    })
-    
-}
-
-// handles display of data to the DOM 
-function renderUI(isLoading, user){
-    console.log("user", user);
-
-        // alert(`rendering...${user.name}`);
-        removeEleChildren(side_bar);
+   // alert(`rendering...${user.name}`);
+    // removeEleChildren(side_bar);
     removeEleChildren(result_container);
-    
 
-
-    if(isLoading){
-        result_container.innerHTML = `<p>Fetching Data..</p>`;
-        return;
+    if (isLoading) {
+      result_container.innerHTML = `<p>Fetching Data..</p>`;
+      return;
     }
 
 
+  if (isLoading === false && result) {
+    console.log("resu", result.user);
+    let user = result.user;
+    let repos = result.repos;
 
-    if(user.status && user.status === "404"){
-        result_container.innerHTML = `<p>User not found!</p>`;
-        return;
+    // let user = result.user;
+
+   
+    // handle user not found 
+    if (user.status && user.status === "404") {
+      result_container.innerHTML = `<p>User not found!</p>`;
+      return;
     }
 
-    // let html = `
-    //     <div>
-    //         <img src=${user.avatar_url}/>
-    //     </div>
-    //     <ul>
-    //         <li>Name: ${user.name}</li>
-    //         <li>Email: ${user.email}</li>
-    //         <li>Company: ${user.company}</li>
-    //         <li>Bio: ${user.bio}</li>
-    //     </ul>
-    // `;
 
+    // add sidebar 
     let sidebar_content = `
           <!-- personal info  -->
           <div>
@@ -145,27 +139,52 @@ function renderUI(isLoading, user){
           </div>
     `;
 
-    console.log("aside", side_bar)
-    side_bar.insertAdjacentHTML("beforeend", sidebar_content)
+    side_bar.insertAdjacentHTML("beforeend", sidebar_content);
+
+    // add repo cards 
+    // let html =  `
+    //   <div class="repo-card">
+    //           <h5>Repo name</h5>
+    //           <p>Repo name</p>
+    //         </div>
+    // `;
+
+    repos.forEach((repo)=>{
+      let html =  `
+      <div class="repo-card">
+              <h5>${repo.name}</h5>
+              <p>${repo.description}</p>
+            </div>
+    `;
+
+    repos_list.insertAdjacentHTML("beforeend", html)
+    });
+
+    // console.log("reeee", repoo.toString())
+
+    // repos_list.innerHTML = repoo;
+
     
 
-    result_container.insertAdjacentElement("beforeend", side_bar)
+    result_container.insertAdjacentElement("beforeend", side_bar);
+        result_container.insertAdjacentElement("beforeend", repos_list);
+
+  }
 }
 
-function removeEleChildren(eleParentNode){
-    while(eleParentNode.firstChild){
-        eleParentNode.removeChild(eleParentNode.firstChild);
-    }
+function removeEleChildren(eleParentNode) {
+  while (eleParentNode.firstChild) {
+    eleParentNode.removeChild(eleParentNode.firstChild);
+  }
 }
 
-// handles first load 
-window.addEventListener("load", ()=>{
-    handleFirstLoad(renderUI);
-})
+// handles first load
+window.addEventListener("load", () => {
+  handleFirstLoad(renderUI);
+});
 
-
-// handles search 
-search_input.addEventListener("keyup", (e)=>{
-    let search_term = e.target.value;
-    handleSearch(search_term, renderUI)
+// handles search
+search_input.addEventListener("keyup", (e) => {
+  let search_term = e.target.value;
+  handleSearch(search_term, renderUI);
 });
